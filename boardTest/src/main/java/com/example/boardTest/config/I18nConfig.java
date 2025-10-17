7bc4359f167d9752a152128308ee3d9c64ef0e05
이지용 (JiYong Lee) <lgy990711@naver.com>
2025-10-17 18:28:33 +0900

    MySql Reloding, create wolrd messages
    header, 브랜치에서 언어 문제 해결 중

---
+++ boardTest/src/main/java/com/example/boardTest/config/I18nConfig.java
@@ -0,0 +1,45 @@
+package com.example.boardTest.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class I18nConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver r = new SessionLocaleResolver();
+        r.setDefaultLocale(Locale.KOREAN); // 기본 ko
+        return r;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor i = new LocaleChangeInterceptor();
+        i.setParamName("lang"); // ?lang=ko|en|uk
+        return i;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setBasenames("messages", "ValidationMessages"); // 확장자/접미사 없이
+        ms.setDefaultEncoding("UTF-8");
+        ms.setFallbackToSystemLocale(false);
+        return ms;
+    }
+}
